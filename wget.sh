00abee255ba016529488fb4d1fac6eb904eb294f
sam bacha <sam@manifoldfinance.com>
2022-03-14 20:48:11 -0700

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -16,4 +16,4 @@ export CMAKE_BUILD_TYPE=Release
 export MAKEFLAGS=-j10
 
 cmake .. -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" $CMAKE_OPTIONS -G "Unix Makefiles" -DTESTS=0 -DSOLC_LINK_STATIC=1 && make
-git ls-files -o build/
+git ls-files -o .
