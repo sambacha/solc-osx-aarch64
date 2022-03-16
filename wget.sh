523f0ba97a0d638f5e020fbf5b19c1cc61ca916a
sam bacha <sam@manifoldfinance.com>
2022-03-15 18:51:45 -0700

    fix(build): bashisms

--- wget.sh
+++ wget.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # https://github.com/ethereum/solidity/releases/download/v0.8.12/solidity_0.8.12.tar.gz
 
 solc_ver=0.8.12
@@ -16,5 +18,6 @@ export CMAKE_BUILD_TYPE=Release
 export MAKEFLAGS=-j10
 
 cmake .. -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" $CMAKE_OPTIONS -G "Unix Makefiles" -DTESTS=0 -DSOLC_LINK_STATIC=1 && make
-git ls-files -o .
-ls -a solidity_0.8.12/
+git ls-files -o solc/
+
+exit 0
