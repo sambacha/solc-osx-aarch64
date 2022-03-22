4b13891822f92ee811e3614475233e3c99189ad9
sam bacha <sam@manifoldfinance.com>
2022-03-22 09:30:17 -0700

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -4,10 +4,10 @@ set -x
 # https://github.com/ethereum/solidity/releases/download/v${SOLC_VERSION}/solidity_${SOLC_VERSION}.tar.gz
 
 #solc_ver=${SOLC_VERSION}
-SOLC_VERSION=0.6.12
+SOLC_VERSION=0.6.3
 
 # wget -q --show-progress --progress=bar -O solidity_${SOLC_VERSION}.tar.gz https://github.com/ethereum/solidity/releases/download/v${SOLC_VERSION}/solidity_${SOLC_VERSION}.tar.gz
-wget -q -O solidity_${SOLC_VERSION}.tar.gz https://github.com/ethereum/solidity/archive/refs/tags/v0.6.12.tar.gz
+wget -q -O solidity_${SOLC_VERSION}.tar.gz https://github.com/ethereum/solidity/archive/refs/tags/v0.6.3.tar.gz
 
 tar xzf solidity_${SOLC_VERSION}.tar.gz
 cd solidity_${SOLC_VERSION}/
