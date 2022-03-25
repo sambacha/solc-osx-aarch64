e4e3234496349ea3320e0f3176484162d079eeca
sam bacha <sam@manifoldfinance.com>
2022-03-25 02:57:50 -0700

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -9,10 +9,15 @@ SOLC_VERSION=0.6.12
 # wget -q --show-progress --progress=bar -O solidity_${SOLC_VERSION}.tar.gz https://github.com/ethereum/solidity/releases/download/v${SOLC_VERSION}/solidity_${SOLC_VERSION}.tar.gz
 wget -q -O solidity_${SOLC_VERSION}.tar.gz https://github.com/ethereum/solidity/archive/refs/tags/v0.6.12.tar.gz
 
-tar xzf solidity_${SOLC_VERSION}.tar.gz
-cd solidity_${SOLC_VERSION}/
-mkdir -p build
-cd build
+tar xzf solidity_0.6.12.tar.gz
+cd solidity_0.6.12/
+echo $PWD
+
+ROOTDIR="$(dirname "$0")/.."
+BUILDDIR="${ROOTDIR}/build"
+BUILD_TYPE=Release
+mkdir -p "${BUILDDIR}"
+cd "${BUILDDIR}"
 
 export BUILD_TYPE=Release
 export CONFIG_SHELL=/bin/bash
@@ -20,11 +25,10 @@ export SOLC_TESTS=Off
 export CMAKE_BUILD_TYPE=Release
 export MAKEFLAGS=-j10
 
-# shellcheck disable=SC2086
-cmake .. -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" $CMAKE_OPTIONS -G "Unix Makefiles" -DTESTS=0 -DSOLC_LINK_STATIC=1 && make -j2
-echo "[BUILD]: Complete"
-git ls-files -o solc/
+cmake .. -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "${@:2}"
+make -j2
 
-# while our scripts pass linting, other scripts may not
-# /home/travis/.travis/job_stages: line 81: secure: unbound
-set +u
+# shellcheck disable=SC2086
+
+echo "[BUILD]: Complete"
+git ls-files -o
